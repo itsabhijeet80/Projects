7de7cf0c5b3355b52f6e3a2a9ab0bcfa7d497077
itsabhijeet80 <94948306+itsabhijeet80@users.noreply.github.com>
2022-09-26 12:04:02 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,31 @@
+def sum(x):
+    sum=0;
+    while(x>0):
+        sum+=x
+        x=x-1
+    return sum
+
+print(sum(25))
+
+
+def sumpro(a,b):
+    sum=a+b
+    pro=a*b
+    return sum,pro
+
+print(sumpro(4,5))
+
+
+def display():
+    print("Hello")
+    print("Bbc")
+
+display()
+
+print(int(3.33))
+
+import math
+x=math.log10(44)
+print(x)
+
+
